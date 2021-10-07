76fe60f8215a5ed96bb6ae8317a15f3ad8553f33
Michael <80859948+bridgemami@users.noreply.github.com>
2021-10-07 03:07:49 +0000

    Initial commit

---
+++ cdn/confirm-box.js
@@ -0,0 +1,31 @@
+function Confirm(title, msg, $true, $false, $link) { /*change*/
+        var $content =  "<div class='dialog-ovelay'>" +
+                        "<div class='dialog'><header>" +
+                         " <h3> " + title + " </h3> " +
+                         "<i class='fa fa-close'></i>" +
+                     "</header>" +
+                     "<div class='dialog-msg'>" +
+                         " <p> " + msg + " </p> " +
+                     "</div>" +
+                     "<footer>" +
+                         "<div class='controls'>" +
+                             " <a class='button button-danger doAction link_me'>" + $true + "</a> " +
+                             " <a class='button button-default cancelAction .link_me'>" + $false + "</a> " +
+                         "</div>" +
+                     "</footer>" +
+                  "</div>" +
+                "</div>";
+         $('body').prepend($content);
+      $('.doAction').click(function () {
+        window.open($link, "_blank"); /*new*/
+        $(this).parents('.dialog-ovelay').fadeOut(500, function () {
+          $(this).remove();
+        });
+      });
+$('.cancelAction, .fa-close').click(function () {
+        $(this).parents('.dialog-ovelay').fadeOut(500, function () {
+          $(this).remove();
+        });
+      });
+      
+   }
